4a0e465ed06174f5b9150fbbc26bff24ac8abfc0
OmarAltabbaa <54678565+OmarAltabbaa@users.noreply.github.com>
2020-12-01 16:20:22 +0000

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -4,7 +4,7 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-char* nafn = "omar altabbaa\0";
+char* nafn = "hallo world\0";
 int stadsetnig=0;
 
 
